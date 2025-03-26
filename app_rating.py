7542bf8f22ec2b2c95859833806513863399341e
bjhardcastle <ben.hardcastle@alleninstitute.org>
2025-03-25 17:59:08 -0700

    Init

---
+++ app_rating.py
@@ -0,0 +1,358 @@
+import datetime
+import json
+import logging
+import time
+from typing import NotRequired, TypedDict
+
+import npc_io
+import panel as pn
+import param
+import polars as pl
+import upath
+import panel.custom 
+
+import db_utils
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)  #! doesn't take effect
+
+# pn.config.theme = 'dark'
+pn.config.notifications = True
+
+
+SIDEBAR_WIDTH = 280
+BUTTON_WIDTH = int(SIDEBAR_WIDTH * 0.8)
+
+if not upath.UPath(db_utils.DB_PATH).exists():
+    db_utils.create_db()
+
+db_df: pl.DataFrame = db_utils.get_db()
+
+
+def get_metrics(qc_path: str):
+    return (
+        db_utils.get_db()
+        .filter(pl.col("qc_path") == qc_path)
+        .select('session_id', 'qc_group', 'plot_name', 'plot_index')
+        .join(
+            other=db_utils.get_session_df(),
+            on="session_id",
+            how="left",
+        )
+        .to_dicts()[0]
+    )
+
+
+def display_metrics(
+    path=str,
+) -> pn.pane.Markdown:
+    metrics = get_metrics(path)
+    stats = f"""
+
+### `{path}`
+"""
+    for k, v in metrics.items():
+        if isinstance(v, float):
+            v = f"{v:.2f}"
+        stats += f"\n{k.replace('_', ' ')}:\n`{v if v else '-'}`\n"
+    return pn.pane.Markdown(stats)
+
+
+def display_rating(
+    path=str,
+) -> pn.pane.Markdown:
+    df = db_utils.get_db().filter(pl.col('qc_path')==path)
+    assert len(df) == 1, f"Expected 1 row, got {len(df)}"
+    row = df.to_dicts()[0]
+    rating: int | None = row["qc_rating"]
+    if rating is None:
+        return pn.pane.Markdown("not yet rated")
+    else:
+        return pn.pane.Markdown(
+            f"**{db_utils.QCRating(rating).name.title()}** ({datetime.datetime.fromtimestamp(row['checked_timestamp']).strftime('%Y-%m-%d %H:%M:%S')})"
+        )
+
+
+def display_item(qc_path: str):
+    filtered_df = db_utils.get_db().filter(pl.col("qc_path") == qc_path)
+    assert len(filtered_df) == 1, "df filtering likely incorrect"
+    path = filtered_df["qc_path"].first()
+    path = upath.UPath(path)
+    assert path is not None, f"Path not stored for {path}"
+    logger.info(f"Displaying {path}")
+    if path.suffix == '.png': 
+        return pn.pane.PNG(
+            path.read_bytes(),
+            sizing_mode="stretch_both",
+        )
+    if path.suffix == '.json':
+        return pn.pane.JSON(
+            json.loads(path.read_text()),
+            sizing_mode="stretch_height",
+        )
+    if path.suffix in ('.txt', '.error'):
+        return pn.pane.Str(
+            path.read_text(),
+            sizing_mode="stretch_height",
+        )
+    if path.suffix == '.link':
+        link = upath.UPath(path.read_text())
+        if link.as_posix().startswith('http'):
+            return pn.pane.HTML(
+                f'<a href="{link.read_text()}">Link</a>',
+                sizing_mode="stretch_height",
+            )
+        if link.as_posix().startswith('s3://'):
+            return pn.pane.Video(
+                npc_io.get_presigned_url(link), loop=False, autoplay=True, width=1600
+            )
+
+        
+
+# initialize for startup, then update as a global variable
+qc_path_generator = db_utils.qc_item_generator(
+    already_checked=False,
+)
+current_qc_path = next(qc_path_generator)
+previous_qc_path: str | None = None
+metrics_pane = display_metrics(current_qc_path)
+qc_rating_pane = display_rating(current_qc_path)
+qc_item_pane = display_item(current_qc_path)
+
+
+def next_path(override_next_path: str | None = None) -> None:
+    global current_qc_path, previous_qc_path
+    if override_next_path:
+        current_qc_path = override_next_path
+        undo_button.disabled = True
+    else:
+        previous_qc_path = current_qc_path
+        undo_button.disabled = False
+        try:
+            current_qc_path = next(qc_path_generator)
+        except StopIteration:
+            pn.state.notifications.warning("No matching paths")
+    qc_item_pane.object = display_item(current_qc_path).object
+    metrics_pane.object = display_metrics(current_qc_path).object
+    qc_rating_pane.object = display_rating(current_qc_path).object
+
+
+def update_and_next(path: str, qc_rating: int) -> None:
+    db_utils.set_qc_rating_for_path(path=path, qc_rating=qc_rating)
+    next_path()
+
+
+# make three buttons for rating qc item
+no_button_text = (
+    f"{db_utils.QCRating.FAIL.name.title()} [{db_utils.QCRating.FAIL.value}]"
+)
+no_button = pn.widgets.Button(name=no_button_text, width=BUTTON_WIDTH)
+no_button.on_click(
+    lambda event: update_and_next(
+        path=current_qc_path, qc_rating=db_utils.QCRating.FAIL
+    )
+)
+yes_button_text = f"{db_utils.QCRating.PASS.name.title()} [{db_utils.QCRating.PASS.value}]"
+yes_button = pn.widgets.Button(name=yes_button_text, width=BUTTON_WIDTH)
+yes_button.on_click(
+    lambda event: update_and_next(
+        path=current_qc_path, qc_rating=db_utils.QCRating.PASS
+    )
+)
+unsure_button_text = f"{db_utils.QCRating.UNSURE.name.title()} [{db_utils.QCRating.UNSURE.value}]"
+unsure_button = pn.widgets.Button(name=unsure_button_text, width=BUTTON_WIDTH)
+unsure_button.on_click(
+    lambda event: update_and_next(
+        path=current_qc_path, qc_rating=db_utils.QCRating.UNSURE
+    )
+)
+skip_button = pn.widgets.Button(name="Skip [s]", width=BUTTON_WIDTH)
+skip_button.on_click(lambda event: next_path())
+undo_button = pn.widgets.Button(name="Previous [p]", width=BUTTON_WIDTH)
+undo_button.disabled = True
+undo_button.on_click(lambda event: next_path(override_next_path=previous_qc_path))
+
+# - ---------------------------------------------------------------- #
+# from https://github.com/holoviz/panel/issues/3193#issuecomment-2357189979
+
+
+# Note: this uses TypedDict instead of Pydantic or dataclass because Bokeh/Panel doesn't seem to
+# like serializing custom classes to the frontend (and I can't figure out how to customize that).
+class KeyboardShortcut(TypedDict):
+    name: str
+    key: str
+    altKey: NotRequired[bool]
+    ctrlKey: NotRequired[bool]
+    metaKey: NotRequired[bool]
+    shiftKey: NotRequired[bool]
+
+
+class KeyboardShortcuts(panel.custom.ReactComponent):
+    """
+    Class to install global keyboard shortcuts into a Panel app.
+
+    Pass in shortcuts as a list of KeyboardShortcut dictionaries, and then handle shortcut events in Python
+    by calling `on_msg` on this component. The `name` field of the matching KeyboardShortcut will be sent as the `data`
+    field in the `DataEvent`.
+
+    Example:
+    >>> shortcuts = [
+        KeyboardShortcut(name="save", key="s", ctrlKey=True),
+        KeyboardShortcut(name="print", key="p", ctrlKey=True),
+    ]
+    >>> shortcuts_component = KeyboardShortcuts(shortcuts=shortcuts)
+    >>> def handle_shortcut(event: DataEvent):
+            if event.data == "save":
+                print("Save shortcut pressed!")
+            elif event.data == "print":
+                print("Print shortcut pressed!")
+    >>> shortcuts_component.on_msg(handle_shortcut)
+    """
+
+    shortcuts = param.List(class_=dict)
+
+    _esm = """
+    // Hash a shortcut into a string for use in a dictionary key (booleans / null / undefined are coerced into 1 or 0)
+    function hashShortcut({ key, altKey, ctrlKey, metaKey, shiftKey }) {
+      return `${key}.${+!!altKey}.${+!!ctrlKey}.${+!!metaKey}.${+!!shiftKey}`;
+    }
+
+    export function render({ model }) {
+      const [shortcuts] = model.useState("shortcuts");
+
+      const keyedShortcuts = {};
+      for (const shortcut of shortcuts) {
+        keyedShortcuts[hashShortcut(shortcut)] = shortcut.name;
+      }
+
+      function onKeyDown(e) {
+        // Ignore key presses in input, textarea, or content-editable elements
+        const target = e.target;
+        const isInput = target.tagName === 'INPUT' || target.tagName === 'TEXTAREA' || target.isContentEditable;
+        if (isInput) {
+          return;
+        }
+        const name = keyedShortcuts[hashShortcut(e)];
+        if (name) {
+          e.preventDefault();
+          e.stopPropagation();
+          model.send_msg(name);
+          return;
+        }
+      }
+
+      React.useEffect(() => {
+        window.addEventListener('keydown', onKeyDown);
+        return () => {
+          window.removeEventListener('keydown', onKeyDown);
+        };
+      });
+
+      return <></>;
+    }
+    """
+
+
+shortcuts = [
+    KeyboardShortcut(name="skip", key="s", ctrlKey=False),
+    KeyboardShortcut(name="previous", key="p", ctrlKey=False),
+] + [
+    KeyboardShortcut(name=k, key=str(v), ctrlKey=False)
+    for k, v in db_utils.QCRating.__members__.items()
+]
+shortcuts_component = KeyboardShortcuts(shortcuts=shortcuts)
+
+
+def handle_shortcut(event):
+    if event.data == "skip":
+        next_path()
+    elif event.data == "previous":
+        next_path(override_next_path=previous_qc_path)
+    else:
+        update_and_next(
+            path=current_qc_path, qc_rating=db_utils.QCRating[event.data]
+        )
+
+
+shortcuts_component.on_msg(handle_shortcut)
+
+# - ---------------------------------------------------------------- #
+
+
+def update_path_generator(event) -> None:
+    global qc_path_generator
+    path_generator_params = dict(
+        qc_rating_filter=qc_rating_filter_radio.value or None,
+        session_id_filter=session_id_filter_dropdown.value or None,
+        plot_name_filter=(
+            plot_name_filter_dropdown.value.split('/')[-1]
+            if plot_name_filter_dropdown.value 
+            else None
+            ),        
+    )
+    print(path_generator_params)
+    if qc_rating_filter_radio.value == "unrated":
+        path_generator_params["already_checked"] = False
+        path_generator_params["qc_rating_filter"] = None
+    elif qc_rating_filter_radio.value == "all":
+        path_generator_params["already_checked"] = None
+        path_generator_params["qc_rating_filter"] = None
+    else:
+        path_generator_params["qc_rating_filter"] = db_utils.QCRating[
+            qc_rating_filter_radio.value.upper()
+        ].value
+    logger.info(f"Updating path generator with {path_generator_params}")
+    qc_path_generator = db_utils.qc_item_generator(**path_generator_params)
+    next_path()
+
+plot_name_filter_dropdown = pn.widgets.Select(
+    name="Plot name",
+    value="",
+    options=db_df.with_columns(pl.concat_str([pl.col('qc_group'), pl.col('plot_name')], separator='/').alias('name'))["name"].unique().sort().to_list() + [""],
+)
+plot_name_filter_dropdown.param.watch(update_path_generator, "value")
+
+session_id_filter_dropdown = pn.widgets.Select(
+    name="Session ID",
+    value="",
+    options=db_df["session_id"].unique().sort(descending=True).to_list() + [""],
+    width=BUTTON_WIDTH,
+)
+session_id_filter_dropdown.param.watch(update_path_generator, "value")
+qc_rating_filter_radio = pn.widgets.RadioBoxGroup(
+    name="Show rated items",
+    options=["all", "unrated"] + [k.lower() for k in db_utils.QCRating.__members__],
+    inline=False,
+    value="unrated",
+)
+qc_rating_filter_radio.param.watch(update_path_generator, "value")
+
+def app():
+    sidebar = pn.Column(
+        metrics_pane,
+        pn.layout.Divider(margin=(20, 0, 15, 0)),
+        pn.pane.Markdown("""**Does the unit's activity drift in or out?**"""),
+        no_button,
+        yes_button,
+        unsure_button,
+        qc_rating_pane,
+        undo_button,
+        skip_button,
+        shortcuts_component,
+        pn.layout.Divider(margin=(20, 0, 15, 0)),
+        pn.pane.Markdown("### Filter"),
+        plot_name_filter_dropdown,
+        qc_rating_filter_radio,
+        session_id_filter_dropdown,
+    )
+
+    return pn.template.MaterialTemplate(
+        site="DR dashboard",
+        title="experiment QC",
+        sidebar=[sidebar],
+        main=[qc_item_pane],
+        sidebar_width=SIDEBAR_WIDTH,
+    )
+
+
+app().servable()
---
+++ db_utils.py
@@ -0,0 +1,243 @@
+import datetime
+import functools
+import logging
+import random
+import time
+from enum import IntEnum
+from typing import Generator
+
+import polars as pl
+import upath
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+QC_DIR = upath.UPath("//allen/programs/mindscope/workgroups/dynamicrouting/qc")
+
+DB_PATH = "//allen/programs/mindscope/workgroups/dynamicrouting/ben/qc_app.parquet"
+
+CACHED_DF_PATH = "s3://aind-scratch-data/dynamic-routing/cache/nwb_components/{}/consolidated/{}.parquet"
+CACHE_VERSION = "v0.0.265"
+
+
+@functools.cache
+def get_session_df() -> pl.DataFrame:
+    return (
+        pl.scan_parquet(CACHED_DF_PATH.format(CACHE_VERSION, "session"))
+        .join(
+            other=pl.scan_parquet(CACHED_DF_PATH.format(CACHE_VERSION, "subject")),
+            on="session_id",
+            how="inner",
+        )
+        .select("session_id", "keywords", "genotype")
+        .collect()
+    )
+
+
+def generate_qc_df() -> pl.DataFrame:
+    paths = [
+        p.as_posix()
+        for p in QC_DIR.rglob("*")
+        if not p.is_dir() and not p.name == "Thumbs.db"
+    ]
+    path_list = pl.col("qc_path").str.split(".").list.get(0).str.split("/").list
+    df = (
+        pl.DataFrame({"qc_path": paths})
+        .with_columns(
+            qc_group=path_list.get(-3).cast(pl.Categorical),
+            plot_name=path_list.get(-2),
+            session_id=path_list.get(-1).str.split("_").list.slice(0, 2).list.join("_"),
+            extension=pl.concat_str(
+                pl.lit("."), (pl.col("qc_path").str.split(".").list.get(-1))
+            ).cast(pl.Categorical),
+        )
+        .with_columns(
+            plot_index=path_list.get(-1)
+            .str.strip_prefix(pl.col("session_id"))
+            .str.strip_prefix("_"),
+        )
+        .with_columns(
+            (
+                pl.when(pl.col("plot_index") == "")
+                .then(pl.lit(None))
+                .otherwise(pl.col("plot_index"))
+                .alias("plot_index")
+                .cast(pl.Int8)
+            ),
+            qc_rating=pl.lit(None).cast(pl.Int8),
+            checked_timestamp=pl.lit(None).cast(pl.Datetime),
+        )
+    )
+    return df
+
+
+class QCRating(IntEnum):
+    FAIL = 0
+    PASS = 1
+    UNSURE = 5
+
+
+class Lock:
+    def __init__(self, path=DB_PATH + ".lock"):
+        self.path = upath.UPath(path)
+
+    def acquire(self):
+        while self.path.exists():
+            time.sleep(0.01)
+        self.path.touch()
+
+    def release(self):
+        upath.UPath(self.path).unlink()
+
+    def __enter__(self):
+        self.acquire()
+        logger.info(f"Acquired lock at {self.path}")
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.release()
+        logger.info(f"Released lock at {self.path}")
+
+
+def create_db(
+    save_path: str = DB_PATH,
+    overwrite: bool = False,
+) -> pl.DataFrame:
+    if upath.UPath(save_path).exists() and not overwrite:
+        raise FileExistsError(
+            f"{save_path} already exists: set overwrite=True to overwrite"
+        )
+    df = generate_qc_df()
+    df.write_parquet(save_path)
+    return df
+
+
+def update_db(db_path: str = DB_PATH) -> None:
+    upath.UPath(
+        db_path + f".backup.{datetime.datetime.now():%Y%m%d_%H%M%S}"
+    ).write_bytes(upath.UPath(db_path).read_bytes())
+    temp_path = db_path + ".temp"
+    # get a brand new df with up-to-date paths
+    new_df = create_db(save_path=temp_path, overwrite=True).join(
+        other=(
+            (original_df := pl.read_parquet(db_path)).select(
+                "qc_rating", "checked_timestamp"
+            )
+        ),
+        on=["session_id", "qc_group", "plot_name", "plot_index"],
+        how="left",
+    )
+    new_df.write_parquet(db_path)
+    logger.info(f"Updated {db_path}: {len(original_df)} -> {len(new_df)} rows")
+    upath.UPath(temp_path).unlink()
+
+
+def reset_db(src_path: str, dest_path: str) -> None:
+    pl.read_parquet(src_path).with_columns(
+        qc_rating=pl.lit(None), checked_timestamp=pl.lit(None)
+    ).write_parquet(dest_path)
+
+
+def get_db(
+    path_filter: str | None = None,
+    already_checked: bool | None = None,
+    qc_group_filter: str | None = None,
+    plot_name_filter: str | None = None,
+    plot_index_filter: int | None = None,
+    extension_filter: str | None = None,
+    session_id_filter: str | None = None,
+    qc_rating_filter: int | None = None,
+    db_path=DB_PATH,
+) -> pl.DataFrame:
+    filter_exprs = []
+    if path_filter:
+        filter_exprs.append(pl.col("qc_path").str.contains(path_filter))
+    if already_checked is True and qc_rating_filter is None:
+        filter_exprs.append(pl.col("qc_rating").is_not_null())
+    elif already_checked is False and qc_rating_filter is None:
+        filter_exprs.append(pl.col("qc_rating").is_null())
+    if session_id_filter:
+        filter_exprs.append(pl.col("session_id").str.starts_with(session_id_filter))
+    if plot_name_filter:
+        filter_exprs.append(pl.col("plot_name") == plot_name_filter)
+    elif qc_group_filter:
+        filter_exprs.append(pl.col("qc_group") == qc_group_filter)
+    if plot_index_filter is not None:
+        filter_exprs.append(pl.col("plot_index") == plot_index_filter)
+    if extension_filter:
+        filter_exprs.append(pl.col("extension") == extension_filter)
+    if qc_rating_filter is not None:
+        filter_exprs.append(pl.col("qc_rating") == int(qc_rating_filter))
+    if filter_exprs:
+        logger.info(
+            f"Filtering qc df with {' & '.join([str(f) for f in filter_exprs])}"
+        )
+    return pl.read_parquet(db_path).filter(*filter_exprs)
+
+
+def qc_item_generator(
+    path_filter: str | None = None,
+    already_checked: bool | None = None,
+    qc_group_filter: str | None = None,
+    plot_name_filter: str | None = None,
+    plot_index_filter: int | None = None,
+    extension_filter: str | None = None,
+    session_id_filter: str | None = None,
+    qc_rating_filter: int | None = None,
+    db_path=DB_PATH,
+) -> Generator[str, None, None]:
+    while True:
+        df = get_db(
+            path_filter=path_filter,
+            already_checked=already_checked,
+            qc_group_filter=qc_group_filter,
+            plot_name_filter=plot_name_filter,
+            plot_index_filter=plot_index_filter,
+            extension_filter=extension_filter,
+            session_id_filter=session_id_filter,
+            qc_rating_filter=qc_rating_filter,
+            db_path=db_path,
+        ).sort(pl.col("session_id").sort(descending=True), "qc_group", "plot_name", "plot_index")
+        if len(df) == 0:
+            raise StopIteration(f"No QC items to check matching filter: {path_filter=}, {already_checked=}, {qc_group_filter=}, {plot_name_filter=}, {plot_index_filter=}, {extension_filter=}, {session_id_filter=}, {qc_rating_filter=}")
+        for row in df.iter_rows(named=True):
+            yield str(row["qc_path"])  # cast to str for mypy
+
+
+def set_qc_rating_for_path(path: str, qc_rating: int, db_path=DB_PATH) -> None:
+    timestamp = int(time.time())
+    original_df = get_db()
+    path_filter = pl.col("qc_path") == path
+    logger.info(f"Updating row for {path} with qc_rating={qc_rating}")
+    df = original_df.with_columns(
+        qc_rating=pl.when(path_filter)
+        .then(pl.lit(qc_rating))
+        .otherwise(pl.col("qc_rating")),
+        checked_timestamp=pl.when(path_filter)
+        .then(pl.lit(timestamp))
+        .otherwise(pl.col("checked_timestamp")),
+    )
+    assert len(df) == len(
+        original_df
+    ), f"Row count changed: {len(original_df)} -> {len(df)}"
+    with Lock():
+        df.write_parquet(db_path)
+    logger.info(f"Overwrote {db_path}")
+
+
+def test_db(with_paths=False):
+    db_path = "test.parquet"
+    create_db(overwrite=True, save_path=db_path, with_paths=with_paths)
+    i = next(qc_item_generator(db_path=db_path))
+    set_qc_rating_for_path(i, 2, db_path=db_path)
+    df = get_db(qc_group_filter=i, already_checked=True, db_path=db_path)
+    assert len(df) == 1, f"Expected 1 row, got {len(df)}"
+    upath.UPath(db_path).unlink()
+
+
+if __name__ == "__main__":
+    # generate_qc_df()
+    create_db(overwrite=True)
+    print(get_db().schema)
+    # update_db()
+    # print(get_df(ks4_filter=True).drop_nulls('path'))
